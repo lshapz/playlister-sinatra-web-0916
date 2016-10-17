5baabea6e6268822b6036f0ffebfca580ee0a58c
lshapz <laurashapiro01@gmail.com>
2016-10-17 16:11:57 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,12 +5,59 @@ class SongsController < ApplicationController
   end
 
 
+  get '/songs/new' do 
+    erb :'songs/new'
+  end 
+
+  post '/songs/new' do
+    #binding.pry
+    @song = Song.create(name: params["Name"])
+    #binding.pry 
+    artist = Artist.find_or_create_by(name: params["Artist Name"])
+    @song.artist = artist   
+    @song.save
+    params["genres"].each do |genre|
+      new_genre = Genre.find_or_create_by(id: genre) 
+      @song.song_genres.build(genre: new_genre) 
+    end
+    #binding.pry
+    @song.save
+    #binding.pry
+    #slug = @song.slug
+    #erb :'songs/show' 
+    redirect to "/songs/#{@song.slug}"
+  end 
+
 
   get '/songs/:slug' do 
     @song = Song.find_by_slug(params[:slug])
     erb :'songs/show'
   end 
 
+  get '/songs/:slug/edit' do 
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/edit'
+  end 
+
+
+  post '/songs/:slug/edit' do 
+    #binding.pry
+    @song = Song.find_by_slug(params[:slug])
+    @song.name = params["Name"]
+    @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
+    
+    @song.save
+    @song.genres.clear 
+
+    params["genres"].each do |genre|
+      new_genre = Genre.find_or_create_by(id: genre)
+      @song.song_genres.build(genre: new_genre)
+    end
+    @song.genres.uniq
+       #binding.pry
+    @song.save
+    redirect to "/songs/#{@song.slug}"
+  end 
 
 
   # get '/songs/:edit' do 
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,5 @@
 class Song < ActiveRecord::Base
   belongs_to :artist 
-  #has_many :songs
   belongs_to :song_genre
   has_many :song_genres
   has_many :genres, through: :song_genres
